2bdc76389e867275d545d62a035ed57b2c48869e
David Ayala and Jason Downing <jason@>
2016-08-09 11:03:23 -0700

    adds switch toggle button to change background and adds .lecture which highlights and includes a border when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+  $("button#switch").click(function() {
+    $("body").toggleClass("dark-background");
+  });
+  $(".lecture").click(function() {
+    $(".lecture").toggleClass("border-highlight");
   });
 });
